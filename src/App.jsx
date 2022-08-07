5b772b6777c1cb2342d5a866e202ca33196f2bb8
Antraxmin <antraxmin@naver.com>
2022-08-07 11:42:53 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <input type="text"/>
+        <button></button>
+    </main>
+  );
+}
+
+export default App;
